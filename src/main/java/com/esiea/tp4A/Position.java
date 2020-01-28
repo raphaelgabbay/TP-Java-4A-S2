2d10ae79d0359921084b5698af6c591686f813c6
AlexCim <alexandrecimonard@gmail.com>
2020-01-28 16:37:04 +0100

    Rover Implementation

---
+++ src/main/java/com/esiea/tp4A/Position.java
@@ -0,0 +1,37 @@
+package com.esiea.tp4A;
+
+public class Position {
+    private int x;
+    private int y;
+    private Direction direction;
+
+    public Position(int x, int y, Direction direction) {
+        this.x = x;
+        this.y = y;
+        this.direction = direction;
+    }
+
+    public int getX() {
+        return x;
+    }
+
+    public void setX(int x) {
+        this.x = x;
+    }
+
+    public int getY() {
+        return y;
+    }
+
+    public void setY(int y) {
+        this.y = y;
+    }
+
+    public Direction getDirection() {
+        return direction;
+    }
+
+    public void setDirection(Direction direction) {
+        this.direction = direction;
+    }
+}
