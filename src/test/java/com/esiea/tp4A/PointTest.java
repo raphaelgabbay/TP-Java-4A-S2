730de11d5812c1b0bc184682657d6ed086d5adba
Ferrier <ferrier.corentin@wanadoo.fr>
2020-02-03 17:50:37 +0100

    Implement second dimension in Point

--- src/test/java/com/esiea/tp4A/PointTest.java
+++ src/test/java/com/esiea/tp4A/PointTest.java
@@ -12,31 +12,43 @@ public class PointTest {
 
     @ParameterizedTest
     @CsvSource({
-        "0,1",
-        "1,2",
-        "-49,-48",
-        "50,-49"
+        "0,0,1",
+        "50,0,-49"
     })
-    void is_point_moved_forward(int a,int expectedResult) {
-        Point p = new Point(a);
-        p.forward();
-        assertThat(p.pos).isEqualTo(expectedResult);
+    void is_point_moved_forward(int posX, int posY,int expectedResult) {
+        Point p = new Point(posX, posY);
+        assertThat(p.goForward()).isEqualTo(expectedResult);
     }
 
     @ParameterizedTest
     @CsvSource({
-        "0,-1",
-        "-1,-2",
-        "5,4",
-        "-49,50"
+        "0,0,-1",
+        "-49,0,50"
     })
-    void is_point_moved_backward(int a,int expectedResult) {
-        Point p = new Point(a);
-        p.backward();
-        assertThat(p.pos).isEqualTo(expectedResult);
+    void is_point_moved_backward(int posX, int posY,int expectedResult) {
+        Point p = new Point(posX, posY);
+        assertThat(p.goBackward()).isEqualTo(expectedResult);
     }
 
+    @ParameterizedTest
+    @CsvSource({
+        "0,0,1",
+        "0,50,-49"
+    })
+    void is_point_moved_right(int posX, int posY, int expectedResult){
+        Point p = new Point(posX, posY);
+        assertThat(p.goRight()).isEqualTo(expectedResult);
+    }
 
+    @ParameterizedTest
+    @CsvSource({
+        "0,0,-1",
+        "0,-49,50"
+    })
+    void is_point_moved_left(int posX, int posY, int expectedResult){
+        Point p = new Point(posX, posY);
+        assertThat(p.goLeft()).isEqualTo(expectedResult);
+    }
 
 
 }
