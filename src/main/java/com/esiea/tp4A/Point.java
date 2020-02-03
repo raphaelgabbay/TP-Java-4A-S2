4784668e3de4678ba89928dd243029e92ff3e238
AlexCim <alexandrecimonard@gmail.com>
2020-02-03 18:07:55 +0100

    Implementation of the map, use it in Point

--- src/main/java/com/esiea/tp4A/Point.java
+++ src/main/java/com/esiea/tp4A/Point.java
@@ -3,35 +3,28 @@ package com.esiea.tp4A;
 public class Point {
     public int posX;
     public int posY;
-    public final int maxPosX;
-    public final int minPosX;
-    public final int maxPosY;
-    public final int minPosY;
-
+    public PlanetMap map;
 
     public Point(int posX, int posY){
         this.posX = posX;
         this.posY = posY;
-        this.maxPosX = 50;
-        this.minPosX = -49;
-        this.maxPosY = 50;
-        this.minPosY = -49;
+        this.map = new PlanetMap();
     }
 
     public int goForward() {
-         return this.posX = (this.posX + 1) > maxPosX ? this.minPosX : this.posX + 1;
+         return this.posX = (this.posX + 1) > map.maxPosX ? map.minPosX : this.posX + 1;
     }
 
     public int goBackward() {
-        return this.posX = (this.posX -1) < minPosX ? this.maxPosX : this.posX - 1;
+        return this.posX = (this.posX -1) < map.minPosX ? map.maxPosX : this.posX - 1;
     }
 
     public int goLeft() {
-        return this.posY = (this.posY - 1 ) < minPosY ? this.maxPosY : this.posY - 1;
+        return this.posY = (this.posY - 1 ) < map.minPosY ? map.maxPosY : this.posY - 1;
     }
 
     public int goRight() {
-        return this.posY = (this.posY + 1 ) > maxPosY ? this.minPosY : this.posY + 1;
+        return this.posY = (this.posY + 1 ) > map.maxPosY ? map.minPosY : this.posY + 1;
     }
 
 }
