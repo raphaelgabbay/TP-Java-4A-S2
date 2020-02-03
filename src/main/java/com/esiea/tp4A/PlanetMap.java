35eb6aba396bbe98d15deff6e3bd345f0d37db75
AlexCim <alexandrecimonard@gmail.com>
2020-02-03 18:07:55 +0100

    Implementation of the map, use it in Point

---
+++ src/main/java/com/esiea/tp4A/PlanetMap.java
@@ -0,0 +1,15 @@
+package com.esiea.tp4A;
+
+public class PlanetMap {
+    public final int maxPosX;
+    public final int minPosX;
+    public final int maxPosY;
+    public final int minPosY;
+
+    public PlanetMap() {
+        this.maxPosX = 50;
+        this.minPosX = -49;
+        this.maxPosY = 50;
+        this.minPosY = -49;
+    }
+}
