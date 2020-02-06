187d2a385b1d655dc4ff975c4de67f3a7a5212e1
Ferrier <ferrier.corentin@wanadoo.fr>
2020-02-06 19:26:47 +0100

    Finalized the movements of the rover
    * moving forward and backward
    * rotate left and right
    * both with tests
    * removed Calc class and tests

--- src/main/java/com/esiea/tp4A/Direction.java
+++ src/main/java/com/esiea/tp4A/Direction.java
@@ -1,8 +1,48 @@
 package com.esiea.tp4A;
 
-public enum Direction {
-    N,
-    S,
-    E,
-    W;
+public enum Direction implements PolarDegreesRotation {
+    N{
+        @Override
+        public Direction rotateLeft() {
+            return W;
+        }
+
+        @Override
+        public Direction rotateRight() {
+            return E;
+        }
+    },
+    S {
+        @Override
+        public Direction rotateLeft() {
+            return E;
+        }
+
+        @Override
+        public Direction rotateRight() {
+            return W;
+        }
+    },
+    E {
+        @Override
+        public Direction rotateLeft() {
+            return N;
+        }
+
+        @Override
+        public Direction rotateRight() {
+            return S;
+        }
+    },
+    W {
+        @Override
+        public Direction rotateLeft() {
+            return S;
+        }
+
+        @Override
+        public Direction rotateRight() {
+            return N;
+        }
+    }
 }
--- src/main/java/com/esiea/tp4A/Position.java
+++ src/main/java/com/esiea/tp4A/Position.java
@@ -1,15 +1,40 @@
 package com.esiea.tp4A;
 
 public class Position {
-    private Point x;
-    private Point y;
-    private Direction direction;
+    public final Point point;
+    public Direction direction;
 
-    public Position(Point x, Point y, Direction direction) {
-        this.x = x;
-        this.y = y;
+    public Position(Point point, Direction direction) {
+        this.point = point;
         this.direction = direction;
     }
 
 
+    public Direction rotateLeft() {
+        return direction = direction.rotateLeft();
+    }
+
+    public Direction rotateRight() {
+        return direction = direction.rotateRight();
+    }
+
+    public Point goForward() {
+        switch (direction) {
+            case N : point.posY = (point.posY + 1) > point.map.maxPosY ? point.map.minPosY : point.posY + 1; break;
+            case E : point.posX = (point.posX + 1) > point.map.maxPosX ? point.map.minPosX : point.posX + 1; break;
+            case S : point.posY = (point.posY - 1) < point.map.minPosY ? point.map.maxPosY : point.posY - 1; break;
+            case W : point.posX = (point.posX - 1) < point.map.minPosX ? point.map.maxPosX : point.posX - 1; break;
+        }
+        return point;
+    }
+
+    public Point goBackward() {
+        switch(direction) {
+            case N : point.posY = (point.posY - 1) < point.map.minPosY ? point.map.maxPosY : point.posY - 1; break;
+            case E : point.posX = (point.posX - 1) < point.map.minPosX ? point.map.maxPosX : point.posX - 1; break;
+            case S : point.posY = (point.posY + 1) > point.map.maxPosY ? point.map.minPosY : point.posY + 1; break;
+            case W : point.posX = (point.posX + 1) > point.map.maxPosX ? point.map.minPosX : point.posX + 1; break;
+        }
+        return point;
+    }
 }
