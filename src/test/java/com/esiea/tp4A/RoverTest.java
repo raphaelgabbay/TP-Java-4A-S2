631c30929e8f079cc0e3035829c91217a61113e8
etienneamiche <etienne.amiche@gmail.com>
2020-02-09 18:55:22 +0100

    Modify Test

--- src/test/java/com/esiea/tp4A/RoverTest.java
+++ src/test/java/com/esiea/tp4A/RoverTest.java
@@ -7,17 +7,21 @@ import org.junit.jupiter.params.provider.CsvSource;
 
 public class RoverTest {
     @ParameterizedTest
-    @CsvSource({
-        "0,0,N,0,1,N"
+    @CsvSource(value = {
+        "0,0,N,fb,0,0,N",
+        "0,0,S,ffl,0,-2,E",
+        "0,0,W,lll,0,0,N",
+        "0,-49,N,bbl,0,49,W",
+        "50,0,E,fl,-49,0,N"
     })
-    void is_rover_moved_right(int posX, int posY, Direction direction, int expectedPosX, int expectedPosY, Direction expectedDirection){
-        //todo : try a way to use an array in CsvSource
-        char [] commands = {'f'};
+    void is_rover_moved_right(int posX, int posY, Direction direction, String stringCommands ,int expectedPosX, int expectedPosY, Direction expectedDirection){
+
         Rover rover = new Rover(generatePosition(posX, posY, direction));
+        char [] commands = stringCommands.toCharArray();
         rover.move(commands);
         assertThat(rover.position.point.posX).isEqualTo(expectedPosX);
         assertThat(rover.position.point.posY).isEqualTo(expectedPosY);
-        assertThat(rover.position.direction).isEqualTo(expectedDirection);
+        assertThat(rover.position.direction).isEqualTo(expectedDirection);a
     }
 
     private Position generatePosition(int posX, int posY, Direction direction) {
