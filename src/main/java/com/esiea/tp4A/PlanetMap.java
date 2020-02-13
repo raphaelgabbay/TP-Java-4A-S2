e5e346dd35140d6585e9dfa6219c4d81cd6c3d63
etienneamiche <etienne.amiche@gmail.com>
2020-02-13 19:29:14 +0100

    Add Obstacle generation to PlanetMap

--- src/main/java/com/esiea/tp4A/PlanetMap.java
+++ src/main/java/com/esiea/tp4A/PlanetMap.java
@@ -1,15 +1,37 @@
 package com.esiea.tp4A;
 
+import java.util.ArrayList;
+import java.util.Random;
+
 public class PlanetMap {
     public final int maxPosX;
     public final int minPosX;
     public final int maxPosY;
     public final int minPosY;
+    public ArrayList<Obstacle> obstacles;
 
-    public PlanetMap() {
+    public PlanetMap(int numberOfObstacles) {
         this.maxPosX = 50;
         this.minPosX = -49;
         this.maxPosY = 50;
         this.minPosY = -49;
+
+        this.generateObstacles(numberOfObstacles);
     }
+
+    private void generateObstacles(int number){
+        this.obstacles = new ArrayList<>();
+        if(number < 0 ) return;
+        while (obstacles.size() != number) {
+            Obstacle obstacle = generateUniqueRandomObstacle();
+            if(!obstacles.contains(obstacle)) obstacles.add(obstacle);
+        }
+    }
+
+    private Obstacle generateUniqueRandomObstacle(){
+        Point p = new Point(Commons.getRandomInt(maxPosX,minPosX),Commons.getRandomInt(maxPosY,minPosY),this);
+        return new Obstacle(p);
+    }
+
+
 }
