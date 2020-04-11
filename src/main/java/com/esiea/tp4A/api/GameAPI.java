478049e61e727888fcdbdb8ba8eeca5f1b458121
Ferrier <ferrier.corentin@wanadoo.fr>
2020-04-11 15:54:54 +0200

    Added game API with first tests

--- src/main/java/com/esiea/tp4A/api/GameAPI.java
+++ src/main/java/com/esiea/tp4A/api/GameAPI.java
@@ -1,5 +1,17 @@
 package com.esiea.tp4A.api;
 
+import com.esiea.tp4A.game.Game;
+import com.esiea.tp4A.game.domain.Position;
+
+import java.util.Set;
+
 public interface GameAPI {
+    Position getPlayerPosition(int playerId);
+    Set<Position> getNearObstaclesPosition(int playerId);
+    Set<Position> getNearOpponentsPosition(int playerId);
+    int getLaserRange();
+    void roverShoot(int playerId);
+    void moveRover(int playerId, String commands);
+    boolean getRoverStatus(int playerId);
 
 }
---
+++ src/main/java/com/esiea/tp4A/game/GameActions.java
@@ -0,0 +1,54 @@
+package com.esiea.tp4A.game;
+
+import com.esiea.tp4A.api.GameAPI;
+import com.esiea.tp4A.game.domain.Position;
+
+import java.util.Optional;
+import java.util.Set;
+import java.util.stream.Collectors;
+
+public class GameActions implements GameAPI {
+    private final Game game;
+
+    public GameActions(Game game) {
+        this.game = game;
+    }
+
+    @Override
+    public Position getPlayerPosition(int playerId) {
+        Optional<Rover> r = game.getMars().getRovers().stream().filter(rover -> rover.getId() == playerId).findFirst();
+        return r.<Position>map(Rover::getPositionRover).orElse(null);
+    }
+
+    @Override
+    public Set<Position> getNearObstaclesPosition(int playerId) {
+        Position playerPosition = getPlayerPosition(playerId);
+        return game.getMars().obstaclePositions().stream().filter(position -> position.getX() < playerPosition.getX() + 30 && position.getX() > playerPosition.getX() - 30 && position.getY() < playerPosition.getY() + 30 && position.getY() > playerPosition.getY() - 30).collect(Collectors.toSet());
+    }
+
+    @Override
+    public Set<Position> getNearOpponentsPosition(int playerId) {
+        Position playerPosition = getPlayerPosition(playerId);
+        return game.getMars().getRovers().stream().filter(rover -> rover.getPositionRover().getX() < playerPosition.getX() + 30 && rover.getPositionRover().getX() > playerPosition.getX() - 30 && rover.getPositionRover().getY() < playerPosition.getY() + 30 && rover.getPositionRover().getY() > playerPosition.getY() - 30).collect(Collectors.toSet()).stream().map(Rover::getPositionRover).collect(Collectors.toSet());
+    }
+
+    @Override
+    public int getLaserRange() {
+        return game.getLaserRange();
+    }
+
+    @Override
+    public void roverShoot(int playerId) {
+        game.getMars().getRovers().stream().filter(rover -> rover.getId() == playerId).findFirst().ifPresent(Rover::shoot);
+    }
+
+    @Override
+    public void moveRover(int playerId, String commands) {
+        game.getMars().getRovers().stream().filter(rover -> rover.getId() == playerId).findFirst().ifPresent(rover -> rover.move(commands));
+    }
+
+    @Override
+    public boolean getRoverStatus(int playerId) {
+        return getPlayerPosition(playerId) != null;
+    }
+}
