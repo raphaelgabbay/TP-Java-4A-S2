13ceba0b3e92b53d2d940c80c6442c9c9d363cb6
Ferrier <ferrier.corentin@wanadoo.fr>
2020-04-12 22:09:58 +0200

    Added GET and PATCH endpoint in controller, fixed gameActions tests

--- src/main/java/com/esiea/tp4A/api/web/PlayerController.java
+++ src/main/java/com/esiea/tp4A/api/web/PlayerController.java
@@ -5,9 +5,7 @@ import com.esiea.tp4A.GameActions;
 import org.jboss.resteasy.annotations.jaxrs.PathParam;
 
 
-import javax.ws.rs.POST;
-import javax.ws.rs.Path;
-import javax.ws.rs.Produces;
+import javax.ws.rs.*;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
 
@@ -20,6 +18,24 @@ public class PlayerController {
     @Produces(MediaType.APPLICATION_JSON)
     @Path("/player/{playerName}")
     public Response authentication(@PathParam("playerName") String playerName) {
-        return gameActions.addPlayer(playerName) ? Response.status(Response.Status.OK).build() : Response.status(Response.Status.CONFLICT).build();
+        return gameActions.addPlayer(playerName) ? Response.status(Response.Status.CREATED).build() : Response.status(Response.Status.CONFLICT).build();
+    }
+
+    @GET
+    @Produces(MediaType.APPLICATION_JSON)
+    @Path("/player/{playerName}")
+    public Response playerStatus(@PathParam("playerName") String playerName) {
+        return gameActions.getPlayerByName(playerName) != -1 ? Response.status(Response.Status.OK).build() : Response.status(Response.Status.NOT_FOUND).build();
+    }
+
+    @PATCH
+    @Produces(MediaType.APPLICATION_JSON)
+    @Path("/player/{playerName}/{command}")
+    public Response playerStatus(@PathParam("playerName") String playerName, @PathParam("command") String command) {
+        if (gameActions.getPlayerByName(playerName) != -1) {
+            gameActions.moveRover(gameActions.getPlayerByName(playerName), command);
+            return Response.status(Response.Status.OK).build();
+        }
+        return Response.status(Response.Status.NOT_FOUND).build();
     }
 }
